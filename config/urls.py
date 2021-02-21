ea1b544f782df209487d7a4100c950656f212881
Rajat Soni <rajat.tcp@gmail.com>
2021-02-21 14:24:43 +0530

    Setup third party app django notifications

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
+from django.conf.urls import url
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
+
+import notifications.urls
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
@@ -29,6 +32,11 @@ urlpatterns += [
     path("api/", include("config.api_router")),
     # DRF auth token
     path("auth-token/", obtain_auth_token),
+]
+
+# Notification URLS
+urlpatterns += [
+    url('^inbox/notifications/', include(notifications.urls, namespace='notifications')),
 ]
 
 if settings.DEBUG:
